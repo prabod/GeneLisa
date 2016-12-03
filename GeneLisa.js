01084569c3de7c741d869aeb21e492ba091658a0
Prabod Rathnayaka <prabod@rathnayaka.me>
2016-12-04 02:02:51 +0530

    Cleaned code and commented

--- GeneLisa.js
+++ GeneLisa.js
@@ -1,32 +1,102 @@
 /**
- * Created by prabod on 11/30/16.
+ # Copyright (C) 2016 Prabod Rathnayaka <prabod@rathnayaka.me>
+ #
+ # This file is part of GeneLisa.
+ #
+ # GeneLisa is free software: you can redistribute it and/or modify
+ # it under the terms of the GNU General Public License as published by
+ # the Free Software Foundation, either version 3 of the License, or
+ # (at your option) any later version.
+ #
+ # Copyright Header is distributed in the hope that it will be useful,
+ # but WITHOUT ANY WARRANTY; without even the implied warranty of
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ # GNU General Public License for more details.
+ #
+ # You should have received a copy of the GNU General Public License
+ # along with Copyright Header.  If not, see <http://www.gnu.org/licenses/>.
+ #
  */
+
+/**
+ * Population for a Generation
+ * */
 var POPULATION = 50;
-var VERTICES = 3;
+
+/**
+ * Number of Vertices for a Polygon
+ * */
+var VERTICES = 4;
+
+/**
+ * Size of a Gene
+ * */
 var GENE_SIZE = 4 + (2 * VERTICES);
+
+/**
+ * Number of Polygons
+ * */
 var POLYGONS = 125;
+
+/**
+ * Length of a Chromosome
+ * */
 var CHROME_SIZE = GENE_SIZE * POLYGONS;
+
+/**
+ * Percentage selected for crossover for next generation
+ * */
 var CROSSOVER_RATE = 0.15;
+
+/**
+ * Percentage that a Chromosome can be mutated
+ * */
 var MUTATION_RATE = 0.01;
+
+/**
+ * Percentage that a Chromosome selected for mutation will be mutated
+ * */
+var MUTATE_AMOUNT = 0.1;
+
+
 var population = [];
-var generation = 0;
+var generation = 0; // Generation of the population
 var goal;
 var workingData = [];
-var mutateAmount = 0.1;
+
+
+/**
+ * Uses ECMASCRIPT6*/
+
+/**
+ * Class that represent a Chromosome
+ * */
 class Chromosome{
+
     constructor(bitString,geneSize,chromoSize){
-        this.bitString = bitString;
+
+        this.valueString = bitString;
         this.geneSize = geneSize;
         this.chromoSize = chromoSize;
         this.c=document.getElementById("workingCanvas");
         this.ctx=this.c.getContext("2d");
         this.vertices = VERTICES;
         this.fitnessValue = 0;
+
     }
 
+    /**
+     * Generate random DNA for the Initial Generation
+     * Value Encoding is used for DNA encoding
+     * DNA = [RED, GREEN, BLUE, ALPHA, X1, Y1, X2, Y2, ...]
+     * */
     randomGenes(){
+
         var bString = [];
         for (var i = 0; i < this.chromoSize; i+= this.geneSize){
+            /**
+             * Generate RGBA
+             * */
             bString.push(
                 Math.random(), // R
                 Math.random(), // G
@@ -34,97 +104,145 @@ class Chromosome{
                 Math.max(Math.random() * Math.random(), 0.2) //A
             );
 
-
+            /**
+             * Generate random (x,y) for vertices
+             * */
             var X = Math.random();
             var Y = Math.random();
+
             for (var j = 0; j < this.vertices ; j++){
+
                 bString.push(
                     X + Math.random() - 0.5,
                     Y + Math.random() - 0.5
-                )
+                );
             }
         }
-        this.bitString = bString;
-    }
-    mutate(rate){
-        for (var i = 0 ; i < this.bitString.length ;i++){
-            var rand = Math.random();
-            if (rand < rate){
-                this.bitString[i] = Math.random()
-            }
-
-        }
+        this.valueString = bString;
     }
 
+    /**
+     * Draw the Chromosome in the canvas
+     * */
     draw(context,width,height){
+        /**
+         * Paint the canvas black to make sure nothing left from the previous draw
+         * */
         context.fillStyle = '#000';
         context.fillRect(0, 0, width, height);
+
+        /**
+         * Draw the Starting Point -> lines to vertices
+         * */
         for(var i = 0; i < this.chromoSize ; i += this.geneSize){
+
             context.beginPath();
+
             context.moveTo(
-                this.bitString[i+4] * width,
-                this.bitString[i+5] * height
+                this.valueString[i + 4] * width,
+                this.valueString[i + 5] * height
             );
+
             for(var j = 0; j < this.vertices - 1; j++){
-                context.lineTo(this.bitString[i+j*2+6] * width,
-                    this.bitString[i+j*2+7] * height)
+                /**
+                 * Draw Lines*/
+                context.lineTo(
+                    this.valueString[i + j * 2 + 6] * width,
+                    this.valueString[i + j * 2 + 7] * height
+                );
             }
+
             context.closePath();
-            var colors = 'rgba(' +
-                ((this.bitString[i] * 255) >> 0) + ',' + // R - int [0,255]
-                ((this.bitString[i + 1] * 255) >> 0) + ',' + // G - int [0,255]
-                ((this.bitString[i + 2] * 255) >> 0) + ',' + // B - int [0,255]
-                this.bitString[i + 3] + ')'; // A - float [0,1]
-            context.fillStyle = colors;
+
+            context.fillStyle = 'rgba(' +
+                ((this.valueString[i    ] * 255) >> 0) + ',' + // R - int [0,255]
+                ((this.valueString[i + 1] * 255) >> 0) + ',' + // G - int [0,255]
+                ((this.valueString[i + 2] * 255) >> 0) + ',' + // B - int [0,255]
+                  this.valueString[i + 3] + ')';
+            //Fill the polygon
             context.fill();
         }
     }
-    fitness(width,height,goal){
+
+    /**
+     * Fitness Function
+     * fitness = 1 -    (Square of pixel difference between chromosome and reference Image)
+     *                  ____________________________________________________________________
+     *                  ( Resolution of the Image * count(RGBA) * Number of Possible Values)
+     *
+     * This fitness function stays inside [0,1]
+     * */
+    fitness(width,height){
+
+        //Draw the Chromosome First
         this.draw(this.ctx,width,height);
+
         var fit = 0;
+
         var imagedata = this.ctx.getImageData(0,0,width,height).data;
 
         for (var i = 0; i < workingData.length;i++){
+
             var dist = workingData[i] - imagedata[i];
+
             fit += dist * dist;
         }
+
         this.fitnessValue = 1 - fit / (75 * 75 * 4 * 256 * 256);
-        //this.ctx.clearRect(0, 0, 350, 350);
+
         return this.fitnessValue;
     }
 }
 
+/**
+ * Cross two Chromosomes and produce a child Chromosome
+ * */
+function crossover(chromosome1,chromosome2,rate,geneSize,chromoSize) {
 
-function crossover(chromesome1,chromosome2,rate,geneSize,chromoSize) {
-    var rand = Math.random();
+    var rand = Math.random(); // Random value for check crossover chance
+
     if (rand < rate){
-        var index = rand * chromesome1.bitString.length >> 0;
-        var bt = [];
+
+        var vString = [];
+
         for (var i =0; i< chromoSize; i += geneSize){
+
             for(var j = 0; j < geneSize; j++){
-                var inheritedGene = (Math.random() < 0.5) ? chromesome1 : chromosome2;
-                var dna = inheritedGene.bitString[i+j];
-                var randon = Math.random();
-                if (randon < MUTATION_RATE){
-                    dna += Math.random() * mutateAmount * 2 - mutateAmount;
+                /**
+                 * Evenly Choose a Parent for breeding*/
+                var inheritedGene = (Math.random() < 0.5) ? chromosome1 : chromosome2;
+
+                var dna = inheritedGene.valueString[i+j];
+
+                var randnum = Math.random(); // Random Number for Mutation chance
+
+                if (randnum < MUTATION_RATE){
+                    /**
+                     * Mutate by some Amount*/
+                    dna += Math.random() * MUTATE_AMOUNT * 2 - MUTATE_AMOUNT;
                 }
+
                 if (dna < 0)
                     dna = 0;
 
                 if (dna > 1)
                     dna = 1;
-                bt.push(dna);
+
+                vString.push(dna);
             }
         }
-        // var bt1 = chromesome1.bitString.slice(0,index).concat(chromosome2.bitString.slice(index));
-        // var bt2 = chromosome2.bitString.slice(0,index).concat(chromesome1.bitString.slice(index));
-        var cr1 =  new Chromosome(bt,geneSize,chromoSize);
-        // var cr2 =  new Chromosome(bt2,geneSize,chromoSize);
-        return cr1;
+        return new Chromosome(vString,geneSize,chromoSize);
     }
-    return (Math.random() < 0.5) ? chromesome1 : chromosome2;
+    /**
+     * If No chance for crossover. Return one of the parent Chromosome*/
+    return (Math.random() < 0.5) ? chromosome1 : chromosome2;
 
 }
+
+/**
+ * Roulette Wheel Function to select two parent for breeding
+ * NEVER USED
+ * a GREEDY METHOD used for selection*/
 function roulette(totalFitness, population) {
     var slice = Math.random() * totalFitness;
     var fitnessSoFar = 0;
@@ -136,17 +254,23 @@ function roulette(totalFitness, population) {
     }
     return population[0];
 }
+
+/**
+ * Initiate GeneLisa.js
+ * */
 function init() {
-        var goalc=document.getElementById("goal");
-        var goalctx=goalc.getContext("2d");
+        /**
+         * Get Reference Canvas and Context*/
+        var goalCanvas=document.getElementById("goal");
+        var goalctx=goalCanvas.getContext("2d");
         var base_image = new Image();
 
+        /**
+         * Draw Reference image on canvas*/
         base_image.onload = function(){
             goalctx.drawImage(base_image,0,0);
-            //goal = goalctx.getImageData(0,0,350,350).data;
-            //console.log(goal);
-            goalc.width = 75;
-            goalc.height = 75;
+            goalCanvas.width = 75;
+            goalCanvas.height = 75;
 
             goalctx.drawImage(base_image,
                 0, 0, 350, 350, 0, 0,
@@ -163,12 +287,16 @@ function init() {
                 workingData[i] = imageData[i];
             }
 
-            goalc.width = 350;
-            goalc.height = 350;
+            goalCanvas.width = 350;
+            goalCanvas.height = 350;
             goalctx.drawImage(base_image, 0, 0);
 
         };
-        base_image.src = './mona.png';
+        base_image.src = './mona.gif';
+
+        /**
+         * Generate Random Chromosomes for the initial generation
+         * */
         for(var i = 0 ; i < POPULATION; i++){
             var cr = new Chromosome([],GENE_SIZE,CHROME_SIZE);
             cr.randomGenes();
@@ -177,50 +305,85 @@ function init() {
 
 }
 
-function tick() {
+/**
+ * Breed a New Generation of Chromosomes
+ * */
+function breed() {
+
     var totalFitness = 0;
     var fittest;
     var fit=0;
-    for(var j = 0 ;j < population.length ; j++){
+
+    /**
+     * Calculate fitness of each and every chromosome
+     * */
+    for(var j = 0 ; j < population.length ; j++){
+
         var temp = population[j].fitness(75,75,goal);
+
         if (temp >= fit){
+
             fit = temp;
-            fittest = population[j];
+            fittest = population[j];// Fittest Chromosome in a Generation
+
         }
+
         totalFitness += temp;
     }
+
+    /**
+     * Sort the Generation
+     * */
     population = population.sort(function(a, b) {
         return b.fitnessValue - a.fitnessValue;
     });
+
     var newPopulation = [];
 
-    /* The number of individuals from the current generation to select for
-     * breeding
-     */
+    /**
+     * Select the Chromosomes with best fitnesses
+     * */
     var selectCount = Math.floor(population.length * CROSSOVER_RATE);
 
-    /* The number of individuals to randomly generate */
+    /**
+     * Number of Chromosomes that needed to be crossed with the each of the Chosen Chromosome
+     * */
     var randCount = Math.ceil(1 / CROSSOVER_RATE);
 
+    /**
+     * Select two parents and breed
+     * */
     for (var i = 0; i < selectCount; i++) {
 
         for (var h = 0; h < randCount; h++) {
-            var randIndividual = i;
 
-            while (randIndividual == i) {
-                randIndividual = (Math.random() * selectCount) >> 0;
+            var parent = i;
+
+            while (parent == i) {
+
+                parent = (Math.random() * selectCount) >> 0;
+
             }
-            var crossed = crossover(population[i],population[randIndividual],CROSSOVER_RATE,GENE_SIZE,CHROME_SIZE);
-            // crossed.mutate(MUTATION_RATE);
+            /**
+             * Breed
+             * */
+            var crossed = crossover(population[i], population[parent], CROSSOVER_RATE, GENE_SIZE, CHROME_SIZE);
             newPopulation.push(crossed);
         }
     }
-    var my =document.getElementById("myCanvas");
-    var ct =my.getContext("2d");
+
+    /**
+     * Draw the fittest Chromosome to Output Canvas
+     * */
+    var myCanvas = document.getElementById("myCanvas");
+    var ct = myCanvas.getContext("2d");
     fittest.draw(ct,350,350);
     population = newPopulation;
     generation ++;
-    console.log(fit);
 
+    /**
+     * update generation and fitness
+     * */
     document.getElementById("gen").innerHTML = generation;
+    document.getElementById("fit").innerHTML = "Fitness = " + fit * 100 + "%";
 }
